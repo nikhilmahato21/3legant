66c54fc681b63d1e9a69fc2dd84eb005f4493980
nikhil <nikhilmahato3337@gmail.com>
2024-11-06 12:59:00 +0530

    bug

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Set this to './' if deploying to a subdirectory or relative path
-});
+})
